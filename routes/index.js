6dd78385b7c2f8da847cdd9d005b6dd33a55ee38
Tim Smith <tjs.nh.smith@gmail.com>
2018-04-22 16:02:16 -0400

    Add Jest. Fix routes with ESLint. Add slack notifications with TravisCI .yml

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const express = require("express"),
-      router  = express.Router();
+const express = require('express'),
+  router = express.Router();
 
 // INDEX ROUTES
 
 // INDEX
-router.get("/", function(req,res){
-  res.render("landing");
+router.get('/', (req, res) => {
+  res.render('landing');
 });
 
 module.exports = router;
